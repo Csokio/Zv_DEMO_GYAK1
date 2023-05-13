e3106b7b444d7376facd161e5fcf91de5667cbd9
Sőregi Krisztián <krisztiansoreg@gmail.com>
2023-05-13 17:59:21 +0200

    commit

--- src/test/java/FunctionTests/PageTest.java
+++ src/test/java/FunctionTests/PageTest.java
@@ -2,9 +2,19 @@ package FunctionTests;
 
 import org.junit.jupiter.api.AfterAll;
 import WebPages.Pages;
+import org.junit.jupiter.api.BeforeEach;
 
 public class PageTest {
 
+    public PageTest()
+    {
+        super();
+    }
+    @BeforeEach
+    public void setup()
+    {
+        new PageTest();
+    }
     @AfterAll
     protected static void closeTheDriver()
     {
